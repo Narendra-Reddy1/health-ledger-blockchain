bf828a7310e935e644a73a2cfca10eaeb11e4a5a
narendra_reddy <narendrareddy54321@gmail.com>
2024-09-11 22:35:15 +0530

    re-committing entire codebase after a huge mess

--- scripts/token.js
+++ scripts/token.js
@@ -1,13 +1,6 @@
 const { ethers, network, getNamedAccounts } = require("hardhat");
 const { PinataSDK } = require("pinata")
 async function name(params) {
-    // const pinata = new PinataSDK({
-    //     pinataJwt: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiIzMGMwZDM2OC04YmUxLTRjY2QtYWVkZS00MWJmYjEyM2I1ZGIiLCJlbWFpbCI6Im5hcmVuZHJhcmVkZHk1NDMyMUBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwicGluX3BvbGljeSI6eyJyZWdpb25zIjpbeyJkZXNpcmVkUmVwbGljYXRpb25Db3VudCI6MSwiaWQiOiJGUkExIn0seyJkZXNpcmVkUmVwbGljYXRpb25Db3VudCI6MSwiaWQiOiJOWUMxIn1dLCJ2ZXJzaW9uIjoxfSwibWZhX2VuYWJsZWQiOmZhbHNlLCJzdGF0dXMiOiJBQ1RJVkUifSwiYXV0aGVudGljYXRpb25UeXBlIjoic2NvcGVkS2V5Iiwic2NvcGVkS2V5S2V5IjoiNDgzYTc0Y2IyMjA5YjkwOWQzNGQiLCJzY29wZWRLZXlTZWNyZXQiOiIxNGYzYmU3YTVlMTI0MTMwZTg1MjZhMWQ1NzhkOGM3M2JjZDI0ODlmNmNmMzQ1YzdiZjc2NmZjNTEzNTk2YzQ2IiwiZXhwIjoxNzU2NzE1MjIxfQ.p7cB68QovafgkM2JXICuplLBB76bZ - dT1WRtdDgmQbw",
-    //     pinataGateway: "teal-eligible-bobolink-804.mypinata.cloud"
-    // })
-
-    // const file = await pinata.gateways.get("QmS6mcz4PTCr9pU7eYxhQA6XU7pXbGoK39AN6YMSDWMvaD");
-    // console.log(file)
 
     const { deployer } = await getNamedAccounts();
     const signer = await ethers.getSigner(deployer);
@@ -17,9 +10,6 @@ async function name(params) {
         value: ethers.parseEther("10")
     });
     await tx.wait();
-    // console.log(await ethers.provider.getBalance("0x4Ceb6D0f29E9aA6318e6e389F07e9204d9ABb6B2"));
-    // console.log(await token.balanceOf("0x4Ceb6D0f29E9aA6318e6e389F07e9204d9ABb6B2")); //source
-    // console.log(await token.balanceOf(" ")); //toAddress
 
     const ledger = await ethers.getContract("HealthLedger", signer);
     console.log(await ledger.getUserStepCount(0, "0x4Ceb6D0f29E9aA6318e6e389F07e9204d9ABb6B2"));
